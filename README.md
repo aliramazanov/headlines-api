14ea58ea2073d662e8431ec93689164e073e01b9
Ali Ramazanov <aliasifzade@gmail.com>
2023-10-27 23:34:53 +0400

    Add schema & connect to DB

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Features
 
-- The Headlines API is a Node.js application built using the Express.js framework. 
+- The Headlines API is a Node.js application built using the Express.js framework.
 - It serves as a powerful and versatile server for managing news articles with full CRUD (Create, Read, Update, Delete) operations.
 - TypeScript is used to ensure type safety, and TypeORM connects the application to a PostgreSQL database.
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import express from "express";
-import dotenv from "dotenv";
 import { Request, Response } from "express";
+import dotenv from "dotenv";
+import { AppDataSource } from "./datasource";
 
 const app = express();
 dotenv.config();
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Connection to databse established!");
+  })
+  .catch(() => {
+    console.error("Error in connection to database!");
+  });
 
 app.get("/api/headlines", (req: Request, res: Response) => {});
 
