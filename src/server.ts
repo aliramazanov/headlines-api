1f05886ff809b7112ca0d832e1626037d4a443fb
Ali Ramazanov <aliasifzade@gmail.com>
2023-10-27 22:47:12 +0400

    Reset Server

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,15 @@
-import dotenv from "dotenv";
 import express from "express";
-import { NewsDataSource } from "./data-source.js";
+import dotenv from "dotenv";
+import { Request, Response } from "express";
 
-dotenv.config();
 const app = express();
-app.use(express.json());
+dotenv.config();
+
+app.get("/api/headlines", (req: Request, res: Response) => {});
 
 const port: number = parseInt(process.env.PORT || "4000", 10);
 const hostname: string = process.env.HOST || "localhost";
 
-NewsDataSource.initialize()
-  .then(() => {
-    console.log("Connected to the DB 📊");
-  })
-  .catch((err: any) => {
-    console.log(`Error in DB connection ${err}`);
-  });
-
-app.get(["/api/newsposts", "/api", "/", ""], async (req, res) => {
-  res.send("Hi there!");
-});
-
 app.listen(port, hostname, () => {
-  console.log(`Server is 🚀 & running on http://${hostname}:${port}`);
+  console.log(`Server is up & running on http://${hostname}:${port}`);
 });
